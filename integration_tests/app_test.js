2e44ad07a91ed5934c183d93d7ebe5718ae219ad
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-06-12 19:13:22 +0200

    docs: step 1

--- integration_tests/app_test.js
+++ integration_tests/app_test.js
@@ -3,6 +3,9 @@ module.exports = {
     browser
       .url('localhost:8080')
       .waitForElementVisible('body', 1000)
+      /* Step 1) Assert that the h1 with text 'Trending Github Languages' is present */
+      /* Try the .assert.containsText(element, text) */
+      /* Run tests with `npm run test:integration` */
       .end();
   }
 };

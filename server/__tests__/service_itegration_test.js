9d30951d81d1b9f33d901ec26d401aeb97068722
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-06-13 06:56:20 +0200

    fix: step 6

--- server/__tests__/service_itegration_test.js
+++ server/__tests__/service_itegration_test.js
@@ -5,8 +5,7 @@ import * as service from "../service";
 describe("Service", () => {
 
   test("should return 20 tweets", () => {
-    // Step 6
-    // Call the service and verify that the results
-    // contains a list of 20 tweets
+    return service.getTrendingLanguages()
+      .then((result) => expect(result.length).toEqual(20));
   });
 });

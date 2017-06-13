85e6d960ef3c8a4065df431276ac68838dc8f47c
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2017-06-13 06:56:04 +0200

    docs: step 6

---
+++ server/__tests__/service_itegration_test.js
@@ -0,0 +1,12 @@
+import sinon from "sinon";
+
+import * as service from "../service";
+
+describe("Service", () => {
+
+  test("should return 20 tweets", () => {
+    // Step 6
+    // Call the service and verify that the results
+    // contains a list of 20 tweets
+  });
+});
